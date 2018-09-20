1b3f796c409ced5ee25c667ef120ee06df4b8561
wzy <wzy@MacBookPro-kerbalwzy.local>
2018-09-20 09:45:37 +0800

    add note

--- LAN_fileServer/main.py
+++ LAN_fileServer/main.py
@@ -4,6 +4,9 @@ import os
 
 
 class MyReConverter(BaseConverter):
+	"""
+	正则路由参数过滤器
+	"""
     def __init__(self, url_map, my_regex):
         super(MyReConverter, self).__init__(url_map)
         self.regex = my_regex
@@ -14,6 +17,10 @@ app.url_map.converters['re'] = MyReConverter
 
 @app.route('/<re(r".*"):file_name>')
 def static_source(file_name):
+	"""
+	返回静态文件的通用视图
+	当配合着Ngixn使用时，该视图不会被调用
+	"""
     if not file_name:
         file_name = 'index.html'
 
@@ -23,6 +30,12 @@ def static_source(file_name):
 
 @app.route('/mulu')
 def get_mulu():
+	"""
+	返回静态文件路由的视图
+	通过在指定的目录下搜索文件，确定所有文件的名称和要被下载时的URL
+	并将数据以JSON的格式返回
+	返回的数据中包含每个文件的文件名称和文件的下载URL
+	"""
     kejian_static_path = '/static/zipFiles/kejians'
     shipin_static_path = '/static/zipFiles/shipins'
     zuoye_static_path = '/static/zipFiles/zuoyes'
@@ -46,4 +59,4 @@ def get_mulu():
 
 
 if __name__ == '__main__':
-    app.run(host="192.168.15.200", port=8000)
+    app.run(host="0.0.0.0"", port=8000)
